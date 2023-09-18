02cafec4f626019f56864579627555a08b4cdd4c
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-17 21:17:42 -0300

    refactor: :recycle: patron redux

--- src/counter-reducer/action/actions.ts
+++ src/counter-reducer/action/actions.ts
@@ -2,3 +2,23 @@ export type CounterAction =
 	| { type: 'increseBy'; payload: { value: number } }
 	| { type: 'decreseBy'; payload: { value: number } }
 	| { type: 'reset' };
+
+export const doIncreseBy = (value: number): CounterAction => {
+	return {
+		type: 'increseBy',
+		payload: { value },
+	};
+};
+
+export const doDecreseBy = (value: number): CounterAction => {
+	return {
+		type: 'decreseBy',
+		payload: { value },
+	};
+};
+
+export const doReset = (): CounterAction => {
+	return {
+		type: 'reset',
+	};
+};
